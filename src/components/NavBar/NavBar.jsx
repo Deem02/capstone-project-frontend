a9a737708fa01f1490c0bb00fd9b99b03aa75814
Deem02 <Deem2002q@gmail.com>
2025-11-01 17:44:34 +0300

    Refactor NavBar to show depend on user role

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,21 +1,34 @@
 import LogOutButton from '../Auth/LogOutButton'
 import { Link } from 'react-router'
 function NavBar({ user, setUser }) {
+  const isAdmin = user && (user.role === 'ADMIN' || user.is_superuser)
   return (
     <nav>
       {
-        user
-          ?
-          <LogOutButton setUser={setUser} />
-          :
+        user ? (
           <>
-            {/* <Link to={'/signup'}>Sign Up</Link> */}
+            <LogOutButton setUser={setUser} />
+            {
+              isAdmin ? (
+                <>
+                  <Link to={'/departments'}> department </Link>
+                  <Link to={'/employees'}> employees </Link>
+                  <Link to={'/tasks'}> tasks </Link>
+                </>
+              ) : (
+                <>
+                  <Link to={'/tasks'}>My Tasks</Link>
+                  <Link to={'/profile'}>My Profile</Link>
+                </>
+
+              )}
+          </>
+        ) : (
+          <>
             <Link to={'/login'}>Log In</Link>
           </>
-      }
-      <Link to={'/departments'}> department </Link>
-      <Link to={'/employees'}> employees </Link>
-      {/* <Link to={'/departments/new'}>Add A department</Link> */}
+        )}
+
     </nav>
   )
 }
