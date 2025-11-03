88538b2a4dc3fbe0db70c70cf8c57b467964954c
Deem02 <Deem2002q@gmail.com>
2025-11-03 17:11:26 +0300

    Add Sass styling to NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,36 +1,44 @@
 import LogOutButton from '../Auth/LogOutButton'
 import { Link } from 'react-router'
+import './NavBar.scss'
 function NavBar({ user, setUser }) {
   const isAdmin = user && (user.role === 'ADMIN' || user.is_superuser)
   return (
-    <nav>
-      {
-        user ? (
-          <>
-            <LogOutButton setUser={setUser} />
-            {
+    <nav className='navbar'>
+      <div className='navbar-container' >
+        <div className='navbar-links'>
+          {
+            user ? (
               isAdmin ? (
                 <>
-                  <Link to={'/departments'}> department </Link>
-                  <Link to={'/employees'}> employees </Link>
-                  <Link to={'/tasks'}> tasks </Link>
+                  <Link to={'/departments'}> Department </Link>
+                  <Link to={'/employees'}> Employees </Link>
+                  <Link to={'/tasks'}> Tasks </Link>
                 </>
               ) : (
                 <>
                   <Link to={'/tasks'}>My Tasks</Link>
                   <Link to={'/profile'}>My Profile</Link>
                 </>
-
-              )}
-          </>
-        ) : (
-          <>
-            <Link to={'/login'}>Log In</Link>
-          </>
-        )}
+              )
+            ) : (
+              <span className='navbar-brand'>Management System</span>
+            )}
+        </div>
+        <div className='navbar-auth'>
+          {user ? (
+            <>
+              <LogOutButton setUser={setUser} />
+            </>
+          ) : (
+            < Link to={'/login'}>Log In</Link>
+          )}
+        </div>
+      </div>
 
     </nav>
   )
 }
+
 
 export default NavBar
